51fd91575e44c4b9199b941311b5b39844e6c1b1
eyu122 <one.man.27.king@gmail.com>
2026-05-29 16:04:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8512645298:AAGAQhbbCXNVsN8JHvoXkoahtVb2gBE9q-c")
-OWNER_ID = int(os.environ.get("OWNER_ID", "5089821980"))
-BASE_URL = os.environ.get("BASE_URL", "")
-SECRET_KEY = os.environ.get("SECRET_KEY", "my-super-secret-key-change-this-1234")
-FILES_DIR = "./files"
+TELEGRAM_TOKEN = "8512645298:AAGAQhbbCXNVsN8JHvoXkoahtVb2gBE9q-c"
+OWNER_ID = 5089821980
+BASE_URL = "https://max7676.pythonanywhere.com"
+SECRET_KEY = "my-super-secret-key-1234"
+FILES_DIR = "/home/Max7676/htmlbot/files"
