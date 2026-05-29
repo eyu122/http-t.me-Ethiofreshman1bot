a7ba95eaf6e51d87f0045d55e7b94c8c3ae1ef3d
eyu122 <one.man.27.king@gmail.com>
2026-05-29 09:39:51 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TELEGRAM_TOKEN = "8512645298:AAGAQhbbCXNVsN8JHvoXkoahtVb2gBE9q-c"
+
+OWNER_ID = 5089821980
+
+BASE_URL = "PASTE_YOUR_RAILWAY_URL_HERE" 
+
+SECRET_KEY = "my-super-secret-key-change-this-1234"
+
+FILES_DIR = "./files"
