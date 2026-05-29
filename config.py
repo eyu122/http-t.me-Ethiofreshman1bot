499d50026d9ce50aa2eb038caaa94a7c061e4676
eyu122 <one.man.27.king@gmail.com>
2026-05-29 10:52:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ TELEGRAM_TOKEN = "8512645298:AAGAQhbbCXNVsN8JHvoXkoahtVb2gBE9q-c"
 
 OWNER_ID = 5089821980
 
-BASE_URL = "PASTE_YOUR_RAILWAY_URL_HERE" 
+BASE_URL = "http-tme-ethiofreshman1bot-production.up.railway.app" 
 
 SECRET_KEY = "my-super-secret-key-change-this-1234"
 
