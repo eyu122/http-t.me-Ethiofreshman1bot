a36c9269840833c02a4757717cef263c1734f07b
eyu122 <one.man.27.king@gmail.com>
2026-05-29 14:43:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-TELEGRAM_TOKEN = "8512645298:AAGAQhbbCXNVsN8JHvoXkoahtVb2gBE9q-c"
+import os
 
-OWNER_ID = 5089821980
-
-BASE_URL = "http-tme-ethiofreshman1bot-production.up.railway.app" 
-
-SECRET_KEY = "my-super-secret-key-change-this-1234"
-
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8512645298:AAGAQhbbCXNVsN8JHvoXkoahtVb2gBE9q-c")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5089821980"))
+BASE_URL = os.environ.get("BASE_URL", "")
+SECRET_KEY = os.environ.get("SECRET_KEY", "my-super-secret-key-change-this-1234")
 FILES_DIR = "./files"
